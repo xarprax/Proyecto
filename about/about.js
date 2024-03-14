f7a0e59f72a758ef207242c26c2efb6e338afd51
Shaktigema <gema.novo@gmail.com>
2024-03-14 20:48:03 +0000

    deberia estar todo, a rezar

--- about/about.js
+++ about/about.js
@@ -19,32 +19,3 @@ function showSlides(n) {
 }
 
 
-// OPINIONS //
-
-document.addEventListener('DOMContentLoaded', function () {
-    let currentReview = 0;
-    const reviews = document.querySelectorAll('.review');
-    console.log('Número de opiniones encontradas:', reviews.length); // Deberías ver la cantidad de opiniones
-
-    function showCurrentReview() {
-        // ... (mantener el resto del código igual)
-    }
-
-    const prevButton = document.getElementById('prev');
-    const nextButton = document.getElementById('next');
-
-    console.log('Prev Button:', prevButton); // Deberías ver el botón prev en la consola
-    console.log('Next Button:', nextButton); // Deberías ver el botón next en la consola
-
-    prevButton.addEventListener('click', function () {
-        console.log('Clicked prev'); // Esto debería aparecer en la consola cuando hagas clic en prev
-        // ... (resto del código del evento prev)
-    });
-
-    nextButton.addEventListener('click', function () {
-        console.log('Clicked next'); // Esto debería aparecer en la consola cuando hagas clic en next
-        // ... (resto del código del evento next)
-    });
-
-    showCurrentReview();
-});
